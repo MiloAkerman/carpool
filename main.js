3c57f3f94ad21cc4029eaeb38dee75c5e8131e29
Milo Akerman <40443620+MiloAkerman@users.noreply.github.com>
2022-01-19 15:58:06 -0500

    Added main files

---
+++ main.js
@@ -0,0 +1,30 @@
+// Import required AWS SDK clients and commands for Node.js.
+import { PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "./libs/awsClient.js";
+
+// Set the parameters
+const params = {
+  Bucket: "carpool-project", // The name of the bucket. For example, 'sample_bucket_101'.
+  Key: "test2.json", // The name of the object. For example, 'sample_upload.txt'.
+  Body: "BODY", // The content of the object. For example, 'Hello world!".
+};
+
+const run = async () => {
+  // Create an object and upload it to the Amazon S3 bucket.
+  try {
+    const results = await s3Client.send(new PutObjectCommand(params));
+    console.log(
+        "Successfully created " +
+        params.Key +
+        " and uploaded it to " +
+        params.Bucket +
+        "/" +
+        params.Key
+    );
+    return results; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+run();
